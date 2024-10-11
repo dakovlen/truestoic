a10508716f69a7e52b143e9932b4e4cd610c9654
Artem Koval <dakovlen@gmail.com>
2024-10-11 21:35:00 +0300

    Add color scheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://truestoic.net/"
+  site: "https://truestoic.net/",
+  integrations: [preact()]
 });
