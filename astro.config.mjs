58fa08f014991453c94741fa8533d7aebdee7686
Artem Koval <dakovlen@gmail.com>
2024-10-18 21:07:00 +0300

    MAIN: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://truestoic.net/",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
