3d99291a90ffacb28523a4a0f5b0c8c8f4d3a74e
Artem Koval <dakovlen@gmail.com>
2024-10-18 21:11:09 +0300

    MAIN: revert analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://truestoic.net/",
-  integrations: [preact()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  integrations: [preact()]
 });
